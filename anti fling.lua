8189e8100066243b82609eff276bce650f361b9c
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-04-20 21:29:52 +0500

    Create anti fling.lua

---
+++ anti fling.lua
@@ -0,0 +1,19 @@
+local lplr = game:GetService("Players").LocalPlayer
+local char = workspace[lplr.Name]
+local tor = char.Torso
+local oldvel = tor.Velocity
+tor.Changed:Connect(function()
+	if tor.Velocity == Vector3.new(0,0,0) then
+		tor.Velocity = oldvel
+	end
+end)
+tor.ChildAdded:Connect(function(v)
+	if v:IsA("BodyForce") then
+		v.Force = Vector3.new(0,0,0)
+		wait()
+		game:GetService("Debris"):AddItem(v,0)
+		repeat
+			wait()
+		until not v
+	end
+end)
