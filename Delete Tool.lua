6a2201e73fa4e5a253978ce359a06f91cc89b5ed
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-04-20 20:47:52 +0500

    fokin regen gon

--- Delete Tool.lua
+++ Delete Tool.lua
@@ -1,4 +1,8 @@
 --Dont steal anything from here pls and also my name is me_kevin3050ti and dc - humanoid#5242
+--note that its not that hard to find the objects so if you want change the deletepos to different (DONT REMOVE THE CFrame.new AND THE BRACKETS)
+--if you have some anti void on, disable it otherwise its really ez to find it
+local deletepos = CFrame.new(-800000,-783321,-951223)
+-- do not edit anything below unless you know what you're doing
 function ivory()
 	game:GetService("Players"):Chat("gear me 0000000000000108158379")
 end
@@ -11,7 +15,7 @@ end
 function move(target)
 	if lp.Character:FindFirstChild("IvoryPeriastron") then
 		local cf = lp.Character.HumanoidRootPart
-		cf.CFrame = CFrame.new(-800000,-900000,490000)
+		cf.CFrame = deletepos
 		wait(0.2)
 		lp.Character.IvoryPeriastron.Remote:FireServer(Enum.KeyCode.E)
 		local looping = true
@@ -19,11 +23,13 @@ function move(target)
 			while true do
 				game:GetService('RunService').Heartbeat:Wait()
 				game.Players.LocalPlayer.Character['Humanoid']:ChangeState(11)
+				target.RotVelocity = Vector3.new(0,0,0)
+		                target.Velocity = Vector3.new(0,0,0)
 				cf.CFrame = target.CFrame * CFrame.new(-1*(target.Size.X/2)-(lp.Character['Torso'].Size.X/2), 0, 0)
 				if not looping then break end
 			end
 		end)
-		spawn(function() while looping do wait(.1) game:GetService("Players"):Chat('unpunish me') end end)
+		spawn(function() while looping do wait(.1) game:GetService("Players"):Chat('unpunish me') end end) -- Do not change the command otherwise it won't attach
 		wait(0.3)
 		looping = false
 		lp.Character.IvoryPeriastron.Remote:FireServer(Enum.KeyCode.E)
@@ -52,6 +58,7 @@ del.Equipped:Connect(function()
 		end
 	end)
 	b1p = m.Button1Up:Connect(function()
+		workspace.FallenPartsDestroyHeight = "nan"
 		local old = lp.Character.HumanoidRootPart.CFrame
 		ivory()
 		repeat wait() until lp.Backpack:FindFirstChild("IvoryPeriastron")
