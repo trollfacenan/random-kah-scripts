6a62a6a67e8bb6e5b8cb52712b1e2632ac934731
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-04-21 16:09:15 +0500

    Update 😎.lua

--- 😎.lua
+++ 😎.lua
@@ -1,1 +1,1 @@
-while wait(0000.0000000001) do for i = 1,2e69 do print("😎😎😎😎😎😎😎😎😎😎😎😎😎😎") game.Players:Chat("😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎 idk why shortcut v1 is so bad and borngggggggggg") end end
+while wait(0000.0000000001) do for i = 1,2e69 do print("😎😎😎😎😎😎😎😎😎😎😎😎😎😎") game.Players:Chat("unff 😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎 idk why shortcut v1 is so bad and borngggggggggg") end end
