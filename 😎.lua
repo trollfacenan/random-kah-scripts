5a3935db43ef3e5e504aa3e1ffaf4aef8e33b6d9
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-04-24 17:11:31 +0500

    Update 😎.lua

--- 😎.lua
+++ 😎.lua
@@ -1,1 +1,4 @@
-while wait(0000.0000000001) do for i = 1,2e69 do print("😎😎😎😎😎😎😎😎😎😎😎😎😎😎") game.Players:Chat("unff 😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎 idk why shortcut v1 is so bad and borngggggggggg") end end
+local clearlogs = true
+while wait(0000.0000000001) do if clearlogs == true then game.Players:Chat("unff 😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎 idk why shortcut v1 is so bad and borngggggggggg") end end
+wait(5)
+clearlogs = false
