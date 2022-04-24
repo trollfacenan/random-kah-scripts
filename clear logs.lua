d3a4bf4d2b3824f9a536bbc65d57ee9fa4ea032b
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-04-24 17:11:51 +0500

    Update and rename 😎.lua to clear logs.lua

---
+++ clear logs.lua
@@ -0,0 +1,4 @@
+local clearlogs = true
+while wait(0000.0000000001) do if clearlogs == true then game.Players:Chat("swag 😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎 idk why shortcut v1 is so bad and borngggggggggg") end end
+wait(5)
+clearlogs = false
--- 😎.lua
+++
@@ -1,4 +0,0 @@
-local clearlogs = true
-while wait(0000.0000000001) do if clearlogs == true then game.Players:Chat("unff 😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎 idk why shortcut v1 is so bad and borngggggggggg") end end
-wait(5)
-clearlogs = false
