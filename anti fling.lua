43612cebd986476f5552814a4b4ca2555e83f84d
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-04-24 17:13:37 +0500

    Update anti fling.lua

--- anti fling.lua
+++ anti fling.lua
@@ -17,3 +17,23 @@ tor.ChildAdded:Connect(function(v)
 		until not v
 	end
 end)
+lplr.CharacterAdded:Connect(function()
+     tor = lplr.Character.Torso
+     oldvel = tor.Velocity
+     tor.ChildAdded:Connect(function(v)
+	if v:IsA("BodyForce") then
+		v.Force = Vector3.new(0,0,0)
+		wait()
+		game:GetService("Debris"):AddItem(v,0)
+		repeat
+			wait()
+		until not v
+	    end
+      end)
+      tor.Changed:Connect(function()
+	if tor.Velocity == Vector3.new(0,0,0) then
+		tor.Velocity = oldvel
+	     end
+	end)
+end)
+end)		
