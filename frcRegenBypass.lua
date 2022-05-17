4cbac3e19780a0f6c29480b85b587f778e9b6a2d
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-05-17 21:44:43 +0500

    Update frcRegenBypass.lua

--- frcRegenBypass.lua
+++ frcRegenBypass.lua
@@ -1,31 +1,23 @@
-for i = 1, 50 do
+plrys = game.Players
+				local looping = true
+				while true do game.RunService.Heartbeat:wait()
 					local jkfv = workspace.Terrain._Game.Admin.Regen
 					local cf = plrys.LocalPlayer.Character.HumanoidRootPart
 					spawn(function()
-						--while true do
-						game:GetService("RunService").Heartbeat:Wait()
-						game.Players.LocalPlayer.Character["Humanoid"]:ChangeState(11)
-						cf.CFrame = jkfv.CFrame * CFrame.new(-1 * (jkfv.Size.X / 2) - (game.Players.LocalPlayer.Character["Torso"].Size.X / 2),0,0)
+						while true do
+							game:GetService("RunService").Heartbeat:Wait()
+							game.Players.LocalPlayer.Character["Humanoid"]:ChangeState(11)
+							cf.CFrame = jkfv.CFrame * CFrame.new(-1 * (jkfv.Size.X / 2) - (game.Players.LocalPlayer.Character["Torso"].Size.X / 2),0,0)
+							if not looping then break end
+						end
 					end)
 					spawn(function()
-						
-						game.Players:Chat("unpunish me")
-						game.Players:Chat("unpunish me")
+						while true do wait(0.1)
+							game.Players:Chat("unpunish me")
+							game.Players:Chat("unpunish me")
+							if not looping then break end
+						end
 					end)
 				end	
-				wait(0.00001)
-				for i = 1, 50 do
-					local jkfv = workspace.Terrain._Game.Admin.Regen
-					local cf = plrys.LocalPlayer.Character.HumanoidRootPart
-					spawn(function()
-						--while true do
-						game:GetService("RunService").Heartbeat:Wait()
-						game.Players.LocalPlayer.Character["Humanoid"]:ChangeState(11)
-						cf.CFrame = jkfv.CFrame * CFrame.new(-1 * (jkfv.Size.X / 2) - (game.Players.LocalPlayer.Character["Torso"].Size.X / 2),0,0)
-					end)
-					spawn(function()
-
-						game.Players:Chat("unpunish me")
-						game.Players:Chat("unpunish me")
-					end)
-				end	
+				wait(1)
+				looping = false
