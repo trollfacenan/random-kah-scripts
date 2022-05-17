78f4758a75836b7134a18faa55fffb019a2fa34d
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-05-17 19:06:15 +0500

    Create frcRegenBypass.lua

---
+++ frcRegenBypass.lua
@@ -0,0 +1,31 @@
+for i = 1, 50 do
+					local jkfv = workspace.Terrain._Game.Admin.Regen
+					local cf = plrys.LocalPlayer.Character.HumanoidRootPart
+					spawn(function()
+						--while true do
+						game:GetService("RunService").Heartbeat:Wait()
+						game.Players.LocalPlayer.Character["Humanoid"]:ChangeState(11)
+						cf.CFrame = jkfv.CFrame * CFrame.new(-1 * (jkfv.Size.X / 2) - (game.Players.LocalPlayer.Character["Torso"].Size.X / 2),0,0)
+					end)
+					spawn(function()
+						
+						game.Players:Chat("unpunish me")
+						game.Players:Chat("unpunish me")
+					end)
+				end	
+				wait(0.00001)
+				for i = 1, 50 do
+					local jkfv = workspace.Terrain._Game.Admin.Regen
+					local cf = plrys.LocalPlayer.Character.HumanoidRootPart
+					spawn(function()
+						--while true do
+						game:GetService("RunService").Heartbeat:Wait()
+						game.Players.LocalPlayer.Character["Humanoid"]:ChangeState(11)
+						cf.CFrame = jkfv.CFrame * CFrame.new(-1 * (jkfv.Size.X / 2) - (game.Players.LocalPlayer.Character["Torso"].Size.X / 2),0,0)
+					end)
+					spawn(function()
+
+						game.Players:Chat("unpunish me")
+						game.Players:Chat("unpunish me")
+					end)
+				end	
