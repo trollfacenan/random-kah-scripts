55ced313173a8b2b6f411035717fd4333f7f4a33
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-06-02 19:26:06 +0500

    Update frcRegenBypass.lua

--- frcRegenBypass.lua
+++ frcRegenBypass.lua
@@ -1,17 +1,19 @@
 --regen attach thingy
-plrys = game.Players
+p = game.Players
+plrys = p
 local target = workspace.Terrain._Game.Admin.Regen
-				local looping = true
-				spawn(function()
-			while true do
-				game:GetService('RunService').Heartbeat:wait()
-				game.Players.LocalPlayer.Character['Humanoid']:ChangeState(11)
-				target.RotVelocity = Vector3.new(0,0,0)
-		                target.Velocity = Vector3.new(0,0,0)
-				cf.CFrame = target.CFrame * CFrame.new(-1*(target.Size.X/2)-(p.LocalPlayer.Character['Torso'].Size.X/2), 0, 0)
-				if not looping then break end
-			end
-		end)
-		spawn(function() while looping do game:GetService('RunService').Heartbeat:wait() game:GetService("Players"):Chat('unpunish me') end end)
-				wait(1)
-				looping = false
+local looping = true
+local cf = p.LocalPlayer.Character.HumanoidRootPart
+spawn(function()
+	while true do
+		game:GetService('RunService').Heartbeat:wait()
+		game.Players.LocalPlayer.Character['Humanoid']:ChangeState(11)
+		target.RotVelocity = Vector3.new(0,0,0)
+		target.Velocity = Vector3.new(0,0,0)
+		cf.CFrame = target.CFrame * CFrame.new(-1*(target.Size.X/2)-(p.LocalPlayer.Character['Torso'].Size.X/2), 0, 0)
+		if not looping then break end
+	end
+end)
+spawn(function() while looping do game:GetService('RunService').Heartbeat:wait() game:GetService("Players"):Chat('unpunish me') end end)
+wait(1)
+looping = false
