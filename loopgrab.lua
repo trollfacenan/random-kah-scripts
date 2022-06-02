2361d5b4de9e44cb336930a481a1af97262239fb
trollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-06-02 16:41:38 +0500

    Update loopgrab.lua

--- loopgrab.lua
+++ loopgrab.lua
@@ -1,14 +1,12 @@
---kinda fast now
-shared.loopgrabbing = false -- value
-
+--yup it will crash on krnl
+shared.loopgrabbing = false
 local pads = workspace.Terrain._Game.Admin.Pads:GetChildren("Head")
 game:GetService("RunService").RenderStepped:Connect(function()
 	if shared.loopgrabbing == false then
 		wait(9e9)
 	end
 	for i, pad in pairs(pads) do
-		spawn(
-			function()
+		spawn(function()
 				repeat wait() until workspace:FindFirstChild(game.Players.LocalPlayer.Name)
 				repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 				firetouchinterest(pad.Head, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
@@ -21,8 +19,7 @@ game:GetService("RunService").Stepped:Connect(function()
 		wait(9e9)
 	end
 	for i, pad in pairs(pads) do
-		spawn(
-			function()
+		spawn(function()
 				repeat wait() until workspace:FindFirstChild(game.Players.LocalPlayer.Name)
 				repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 				firetouchinterest(pad.Head, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
@@ -35,8 +32,7 @@ game:GetService("RunService").Heartbeat:Connect(function()
 		wait(9e9)
 	end
 	for i, pad in pairs(pads) do
-		spawn(
-			function()
+		spawn(function()
 				repeat wait() until workspace:FindFirstChild(game.Players.LocalPlayer.Name)
 				repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
 				firetouchinterest(pad.Head, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
