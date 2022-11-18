4d88bfc2f35d6bb2d9cfca7c1e27bbfb5addfd30
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-11-18 23:39:02 +0500

    Update perm.lua

--- perm.lua
+++ perm.lua
@@ -1,6 +1,6 @@
 shared.Perm = true
 local Admin = workspace.Terrain._Game.Admin
-while true do
+game:GetService("RunService").RenderStepped:Connect(function()
 if not Admin.Pads:FindFirstChild(game:service'Players'.LocalPlayer.Name.."'s admin") then
 if Admin.Pads:FindFirstChild("Touch to get admin") then
 firetouchinterest(game:service'Players'.LocalPlayer.Character:WaitForChild("Left Leg",1337*10),Admin.Pads["Touch to get admin"].Head,1)
@@ -11,6 +11,5 @@ fireclickdetector(Admin.Regen.ClickDetector,0)
 end
 end
 end
-game:service'RunService'.Heartbeat:Wait()
 if shared.Perm == false then break end
-end
+end)
