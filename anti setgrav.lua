ce55ef21f5410e98a0c50549a4cb4c4c2b098c47
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-11-18 23:35:39 +0500

    Update anti setgrav.lua

--- anti setgrav.lua
+++ anti setgrav.lua
@@ -1,13 +1,22 @@
-if v:IsA("BodyForce") then
-			v.Force = Vector3.new(0,0,0)
-			v:GetPropertyChangedSignal("Force"):Connect(function()
-				if v.Force ~= Vector3.new(0,0,0) then
-					v.Force = Vector3.new(0,0,0)
-				end
-			end)
-			game:GetService("RunService").RenderStepped:Wait()
-			game:GetService("Debris"):AddItem(v,0)
-			repeat
-				wait()
-			until not v
-		end
+local plr = game:GetService("Players")
+local lplr = plr.LocalPlayer
+local char = lplr.Character
+lplr.CharacterAdded:Connect(function(it)
+    task.wait(.1)
+    char = it
+end) 
+char.DescendantAdded:Connect(function(v)
+    if v:IsA("BodyForce") then
+        v.Force = Vector3.new(0,0,0)
+        v:GetPropertyChangedSignal("Force"):Connect(function()
+            if v.Force ~= Vector3.new(0,0,0) then
+                v.Force = Vector3.new(0,0,0)
+            end
+        end)
+        game:GetService("RunService").RenderStepped:Wait()
+        game:GetService("Debris"):AddItem(v,0)
+        repeat
+            task.wait()
+        until not v 
+    end
+end)
