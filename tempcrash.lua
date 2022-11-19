97dd4a29203c466cc0a4278370439f4a5fc1e120
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-11-19 14:23:27 +0500

    Delete tempcrash.lua

--- tempcrash.lua
+++
@@ -1,8 +0,0 @@
-for i = 1,3 do 
-			game:GetService("Players"):Chat("size all 0.3")
-end
-game:GetService("Players"):Chat("freeze all")
-for i = 1,3 do
-			game:GetService("Players"):Chat("size all 10")
-end
-game:GetService("Players"):Chat("clone all")
