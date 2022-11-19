031bc3ae2d2c46540eb24080f093463b087beb1a
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-11-19 14:17:05 +0500

    Update RegenAttach.lua

--- RegenAttach.lua
+++ RegenAttach.lua
@@ -1,19 +1,24 @@
 -- Script that attaches to the admin reset (regen) part.
 p = game.Players
 lplr = p.LocalPlayer
+local chatbox = Instance.new("TextBox", workspace)
+local function chat(msg)
+    chatbox:SetTextFromInput(msg)
+    p:Chat(msg)
+end
 local part = workspace.Terrain._Game.Admin.Regen
 local looping = true
-local cf = p.LocalPlayer.Character.HumanoidRootPart
-task.spawn(function()
+local cf = lplr.Character.HumanoidRootPart
+coroutine.wrap(function()
 	while true do
 		game:GetService("RunService").RenderStepped:Wait()
 		lplr.Character:FindFirstChildOfClass("Humanoid"):ChangeState(11)
 		part.RotVelocity = Vector3.new(0,0,0)
-		part.Velocity = Vector3.new(0,0,0)
+                part.Velocity = Vector3.new(0,0,0)
 		cf.CFrame = part.CFrame * CFrame.new(-1*(part.Size.X/2)-(lplr.Character.Torso.Size.X/2), 0, 0)
 		if not looping then break end
 	end
-end)
-spawn(function() while looping do game:GetService('RunService').RenderStepped:Wait() game:GetService("Players"):Chat('unpunish me') end end)
+end)()
+coroutine.wrap(function() while looping do game:GetService('RunService').RenderStepped:Wait() chat('unpunish me') end end)()
 task.wait(1)
 looping = false
