e05daf23e99ac589dd3b191d3ec213d3bbf88456
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-11-21 14:11:32 +0500

    Update anti fly.lua

--- anti fly.lua
+++ anti fly.lua
@@ -1,10 +1,9 @@
 local antifly = true
 local lp = game:GetService("Players").LocalPlayer
-while true do
-	game:GetService("RunService").Heartbeat:Wait()
+game:GetService("RunService").RenderStepped:Connect(function()
 	for i,s in pairs(lp:GetDescendants()) do
 		if s.Name == "Unfly" then
 			if antifly == true then s.Parent:Destroy() lp.Character.Humanoid.PlatformStand = false for i,v in pairs(lp.Character.Torso:GetChildren()) do if v.Name:find("Body") then v:Destroy() end end end
 		end
 	end
-end
+end)
