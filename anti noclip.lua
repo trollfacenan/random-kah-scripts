ed4775b58914f6ceecbd01f2441adff388f548e8
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-11-21 14:15:22 +0500

    Update anti noclip.lua

--- anti noclip.lua
+++ anti noclip.lua
@@ -10,19 +10,25 @@ c:WaitForChild("Humanoid").Changed:Connect(function()
         c.Humanoid.PlatformStand = false
     end
 end)
+if c.Humanoid.PlatformStand == true then
+     c.Humanoid.PlatformStand = false
+end
+if c.Torso.Anchored == true then
+     c.Torso.Anchored = false
+end
 v.PlayerGui.ChildAdded:Connect(function()
     if v.Name == "NoClip" then
         v.Disabled = true
         v:GetPropertyChangedSignal("Disabled"):Connect(function()
             v.Disabled = true
         end)    
-        wait()
+        task.wait()
         v:Destroy()
         repeat wait() until not v
     end
 end)  
 while true do
-    game:GetService("RunService").Heartbeat:Wait()
+    game:GetService("RunService").RenderStepped:Wait()
     if v.PlayerGui:FindFirstChild("NoClip") then
         v.PlayerGui.NoClip.Disabled = true
         v.PlayerGui.NoClip:Destroy()
