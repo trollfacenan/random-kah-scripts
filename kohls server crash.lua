93fff4f23627cbfe3b785a5ee14976844bf782b9
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2022-12-27 20:40:18 +0500

    Update kohls server crash.lua

--- kohls server crash.lua
+++ kohls server crash.lua
@@ -1,10 +1,13 @@
-while task.wait(.2) do
-    for i = 1,3 do 
+repeat
+    task.wait(0.2)
+    for i = 1, 3 do
+	game:GetService("RunService").Heartbeat:Wait()
 	game:GetService("Players"):Chat("size all 0.3")
     end
     game:GetService("Players"):Chat("freeze all "..math.random(240000,300000))
-    for i = 1,3 do
+    for i = 1, 3 do
+	game:GetService("RunService").Heartbeat:Wait()
 	game:GetService("Players"):Chat("size all 10")
     end
     game:GetService("Players"):Chat("clone all")
-end
+until nil
