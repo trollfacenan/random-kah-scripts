f47974270164bf7d1ff7d3eb2ee11ee857194501
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2023-02-10 20:22:04 +0500

    Update AntiSetgrav.lua

--- AntiSetgrav.lua
+++ AntiSetgrav.lua
@@ -1,11 +1,16 @@
 local plr = game:GetService("Players")
 local lplr = plr.LocalPlayer
 local char = lplr.Character
+local connections
 lplr.CharacterAdded:Connect(function(it)
-    task.wait(.1)
+    for i,v in pairs(connections) do
+        if v and v.Disconnect then
+            v:Disconnect()
+        end
+    end        
     char = it
-end) 
-char.DescendantAdded:Connect(function(v)
+     --skid my own code speedrun
+    table.insert(connections, char.DescendantAdded:Connect(function(v)
     if v:IsA("BodyForce") then
         v.Force = Vector3.new(0,0,0)
         v:GetPropertyChangedSignal("Force"):Connect(function()
@@ -19,4 +24,20 @@ char.DescendantAdded:Connect(function(v)
             task.wait()
         until not v 
     end
-end)
+end))
+end) 
+table.insert(connections, char.DescendantAdded:Connect(function(v)
+    if v:IsA("BodyForce") then
+        v.Force = Vector3.new(0,0,0)
+        v:GetPropertyChangedSignal("Force"):Connect(function()
+            if v.Force ~= Vector3.new(0,0,0) then
+                v.Force = Vector3.new(0,0,0)
+            end
+        end)
+        game:GetService("RunService").RenderStepped:Wait()
+        game:GetService("Debris"):AddItem(v,0)
+        repeat
+            task.wait()
+        until not v 
+    end
+end))
