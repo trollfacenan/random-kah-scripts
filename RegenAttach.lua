ab5ba3faab1a5bf0427d24ccc3b1123299a054a1
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2023-06-02 17:39:08 +0500

    Update RegenAttach.lua

--- RegenAttach.lua
+++ RegenAttach.lua
@@ -1,24 +1,23 @@
 -- Script that attaches to the admin reset (regen) part.
-p = game.Players
-lplr = p.LocalPlayer
-local chatbox = Instance.new("TextBox", workspace)
+local p = game:GetService("Players")
+local lplr = p.LocalPlayer
 local function chat(msg)
-    chatbox:SetTextFromInput(msg)
     p:Chat(msg)
 end
 local part = workspace.Terrain._Game.Admin.Regen
 local looping = true
 local cf = lplr.Character.HumanoidRootPart
+chat("Hi-hi-hi, Mr. Gore")
 coroutine.wrap(function()
 	while true do
-		game:GetService("RunService").RenderStepped:Wait()
 		lplr.Character:FindFirstChildOfClass("Humanoid"):ChangeState(11)
 		part.RotVelocity = Vector3.new(0,0,0)
                 part.Velocity = Vector3.new(0,0,0)
 		cf.CFrame = part.CFrame * CFrame.new(-1*(part.Size.X/2)-(lplr.Character.Torso.Size.X/2), 0, 0)
 		if not looping then break end
+	        task.wait()
 	end
 end)()
-coroutine.wrap(function() while looping do game:GetService('RunService').RenderStepped:Wait() chat('unpunish me') end end)()
+coroutine.wrap(function() while looping do chat('unpunish me '..tostring(math.random(10,1024)) task.wait() end end)()
 task.wait(1)
 looping = false
