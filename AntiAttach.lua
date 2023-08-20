9dd6b7d3c011f8312f75cb8b4e89ac93d204c430
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2023-08-20 15:25:13 +0500

    Update AntiAttach.lua

--- AntiAttach.lua
+++ AntiAttach.lua
@@ -1,19 +1,18 @@
 shared.antiattach = true
 local plrs = game:GetService("Players")
-local chatbox = Instance.new("TextBox", workspace)
 local function chat(msg)
-    chatbox:SetTextFromInput(msg)
     plrs:Chat(msg)
 end
 local lp = plrs.LocalPlayer
 local ws = workspace
-game:GetService("RunService").RenderStepped:Connect(function()
-	if shared.antiattach == false then wait(20e20*20e20*20696969696969) end
+local hi
+hi = game:GetService("RunService").RenderStepped:Connect(function()
+	if shared.antiattach == false then hi:Disconnect() end
 	for a,plr in pairs(plrs:GetPlayers()) do
 		if plr.Name ~= lp.Name then
 			repeat game:GetService("RunService").RenderStepped:Wait() until ws:FindFirstChild(plr.Name)
 			for i,i2 in pairs(plr.Character:GetDescendants()) do
-				if i2:IsA"Weld" or i2:IsA"Snap" then
+				if i2:IsA"Weld" and not i2.Name:lower():find"left" and not i2.Name:lower():find"right" and not i2.Name == "Neck" or i2:IsA"Snap" then
 					chat(":refresh "..dsd.Parent.Parent.Name)
 				end
 				task.wait()
