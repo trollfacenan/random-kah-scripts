6fd826d41a6fc4306a910e7a3b51e57696fae144
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2024-03-29 21:06:08 +0500

    Update loopgrab.lua

--- loopgrab.lua
+++ loopgrab.lua
@@ -1,41 +1,14 @@
-shared.loopgrabbing = false
+print("Don't use this")
 local c1
 local c2
 local c3
-local pads = workspace.Terrain._Game.Admin.Pads:GetChildren()
+local pads = "Don't use this"
 c1 = game:GetService("RunService").RenderStepped:Connect(function()
-	if shared.loopgrabbing == false then
-		c1:Disconnect()
-	end
-	for i, pad in pairs(pads) do
-		task.spawn(function()
-				repeat task.wait() until workspace:FindFirstChild(game.Players.LocalPlayer.Name)
-				repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-				firetouchinterest(pad.Head, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
-			end)
-	end
+	print("Don't use this")
 end)
 c2 = game:GetService("RunService").Stepped:Connect(function()
-	if shared.loopgrabbing == false then
-		c2:Disconnect()
-	end
-	for i, pad in pairs(pads) do
-		task.spawn(function()
-				repeat task.wait() until workspace:FindFirstChild(game.Players.LocalPlayer.Name)
-				repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-				firetouchinterest(pad.Head, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
-			end)
-	end
+	print("Don't use this")
 end)
 c3 = game:GetService("RunService").Heartbeat:Connect(function()
-	if shared.loopgrabbing == false then
-		c3:Disconnect()
-	end
-	for i, pad in pairs(pads) do
-		task.spawn(function()
-				repeat task.wait() until workspace:FindFirstChild(game.Players.LocalPlayer.Name)
-				repeat task.wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-				firetouchinterest(pad.Head, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
-			end)
-	end
+	print("Don't use this")
 end)
