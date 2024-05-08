7cb2fd79c623d62ff27962a48f61377d6500dc4e
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2024-05-08 19:33:17 +0500

    Update KohlsPlus.lua

--- KohlsPlus.lua
+++ KohlsPlus.lua
@@ -1,7 +1,7 @@
--- Doesn't have "others" or "all" support but I'll add that later.
+-- Aliases are temporarily disabled because they were bugging the script.
 
 local function GetPlayer(text)
-	for _,Player in game:GetService("Players") do
+	for _,Player in game:GetService("Players"):GetPlayers() do
 		if string.sub(string.lower(Player.Name),1,string.len(text)) == string.lower(text) then
 			return Player
 		end
@@ -22,16 +22,14 @@ local function run(message)
 end
 
 if not game:GetService("RunService"):IsStudio() then
-	assert(game.PlaceId == 112420803, "Unsupported game")
-	assert(getgenv, "Cannot load Kohls+, getgenv is missing.")
-	assert(typeof(Instance.new("Part")) == "Instance", "\nUnknown error, failed to run Kohls+\nTry rejoining or try again later.")
-	assert(game:GetService("HttpService"):JSONDecode(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/trollfacenan/random-kah-scripts/main/run.json")).works, "Kohls+ is under maintenance, please try again later.")
+	assert(game:GetService("HttpService"):JSONDecode(request({Url = "https://raw.githubusercontent.com/trollfacenan/random-kah-scripts/main/run.json", Method = "GET"}).Body).works, "Kohls+ is under maintenance, please try again later.")
 	if KohlsPlus then
 		return
 	end
 	getgenv().KohlsPlus = true
 	getgenv().syn = nil
 	if not run_on_actor then
+		-- obsolete
 		script.Parent = Instance.new("Actor")
 		settingsTable.actor_missing = true
 	end
@@ -50,11 +48,27 @@ local firetouchinterestc = function(part, touchedby, idk) -- idk if this works b
 	local fakeclone
 	if part:IsA("BasePart") then
 		pcall(function()
+			local oldt = part.Transparency
 			part.Archivable = true
-			fakeclone = part:Clone()
-			fakeclone:PivotTo(touchedby.CFrame)
+			local fake = part:Clone()
+			fake.Name = math.random()
+			fake.Parent = part.Parent
+			part.Transparency = 1
+			local old = part:GetPivot()
+			local olda = part.Anchored
+			local oldc = part.CanCollide
+			part.Anchored = true
+			part.CanCollide = false
+			part:PivotTo(touchedby.CFrame)
 			task.wait()
-			fakeclone:Destroy()
+			part.Anchored = olda
+			part.CanCollide = oldc
+			part:PivotTo(old)
+			olda = nil
+			oldc = nil
+			part.Transparency = oldt
+			fake:Destroy()
+			fake = nil
 		end)
 	elseif part:IsA("TouchTransmitter") then
 		local basepart
@@ -62,12 +76,29 @@ local firetouchinterestc = function(part, touchedby, idk) -- idk if this works b
 			basepart = part.Parent
 			task.wait()
 		until basepart:IsA("BasePart")
+		part = basepart
 		pcall(function()
+			local oldt = part.Transparency
 			part.Archivable = true
-			fakeclone = basepart:Clone()
-			fakeclone:PivotTo(touchedby.CFrame)
+			local fake = part:Clone()
+			fake.Name = math.random()
+			fake.Parent = part.Parent
+			part.Transparency = 1
+			local old = part:GetPivot()
+			local olda = part.Anchored
+			local oldc = part.CanCollide
+			part.Anchored = true
+			part.CanCollide = false
+			part:PivotTo(touchedby.CFrame)
 			task.wait()
-			fakeclone:Destroy()
+			part.Anchored = olda
+			part.CanCollide = oldc
+			part:PivotTo(old)
+			olda = nil
+			oldc = nil
+			part.Transparency = oldt
+			fake:Destroy()
+			fake = nil
 		end)
 	end
 end
@@ -123,6 +154,13 @@ commands = {
 	["ban"] = {
 		name = "ban",
 		action = function(who : string)
+			if not who then
+				game:GetService("StarterGui"):SetCore("SendNotification", {
+					Title = "Error",
+					Text = "Please specify a player"
+				})
+				return
+			end
 			if settingsTable.banned[GetPlayer(who)] then
 				game:GetService("StarterGui"):SetCore("SendNotification", {
 					Title = "Error",
@@ -139,12 +177,19 @@ commands = {
 					Text = "Why are you trying to ban yourself?"
 				})
 			end
- 		end,
+		end,
 		desc = "Makes the player unable to join the server until unbanned."
 	},
 	["unban"] = {
 		name = "unban",
 		action = function(who : string)
+			if not who then
+				game:GetService("StarterGui"):SetCore("SendNotification", {
+					Title = "Error",
+					Text = "Please specify a player"
+				})
+				return
+			end
 			if GetPlayer(who) ~= game:GetService("Players").LocalPlayer and settingsTable.banned[GetPlayer(who)] then
 				run("respawn "..GetPlayer(who).Name)
 				run("pm "..GetPlayer(who).Name.." You have been unbanned 🙂")
@@ -161,6 +206,13 @@ commands = {
 	["kick"] = {
 		name = "kick",
 		action = function(who : string)
+			if not who then
+				game:GetService("StarterGui"):SetCore("SendNotification", {
+					Title = "Error",
+					Text = "Please specify a player"
+				})
+				return
+			end
 			if GetPlayer(who) ~= game:GetService("Players").LocalPlayer then
 				local plr = GetPlayer(who)
 				local name = plr.Name
@@ -248,58 +300,130 @@ commands = {
 			settingsTable.perm = not settingsTable.perm
 		end,
 		desc = "Gives you permanent admin pad (to toggle use this command.)",
+	},
+	["shutdown"] = {
+		name = "shutdown",
+		action = function(method : string?)
+			-- only vg ATM, more methods later
+			if method and method:lower() == "vg" or not method then
+				if game:GetService("Players").LocalPlayer.Character.Parent ~= workspace then
+					return game:GetService("StarterGui"):SetCore("SendNotification", {
+						Title = "Error",
+						Text = "Cannot crash while punished, try another method. For methods, use -methods"
+					})
+				end
+				run("gear me 94794847")
+				repeat task.wait() until game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("VampireVanquisher")
+				game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("VampireVanquisher").Parent = game:GetService("Players").LocalPlayer.Character
+				task.wait()
+				run("unsize me me me")
+				task.wait()
+				run("unsize me me me")
+				if game:GetService("Players").LocalPlayer.Character:FindFirstChild("VampireVanquisher") then
+					game:GetService("StarterGui"):SetCore("SendNotification", {
+						Title = "Success",
+						Text = "Crashed server"
+					})
+				else
+					game:GetService("StarterGui"):SetCore("SendNotification", {
+						Title = "Failed",
+						Text = "Failed to crash server, anticrash is possibly enabled"
+					})
+				end
+			end
+		end,
+		desc = "Shutdowns/crashes the server you are on.",
+		aliases = {"crash", "stopserver"}
+	},
+	["kick2"] = {
+		name = "seedkick",
+		action = function(target: string)
+			if not target then
+				game:GetService("StarterGui"):SetCore("SendNotification", {
+					Title = "Error",
+					Text = "Please specify a player"
+				})
+				return
+			end
+			if GetPlayer(target) == game:GetService("Players").LocalPlayer then
+				game:GetService("StarterGui"):SetCore("SendNotification", {
+					Title = "Error",
+					Text = "Cannot kick yourself"
+				})
+				return
+			end
+			-- not mine (made by digitality)
+			run("freeze "..GetPlayer(target).Name)
+			task.wait()
+			game:GetService("Players").LocalPlayer.Character:PivotTo(GetPlayer(target).Character:GetPivot())
+			for i = 1, 100 do
+				run("gear me 1645056094")
+			end
+			for _, v in game:GetService("Players").LocalPlayer:FindFirstChildOfClass("Backpack"):GetChildren() do
+				v.Parent = game:GetService("Players").LocalPlayer.Character
+				task.wait()
+				v:Activate()
+			end
+			run("punish me")
+			task.wait(.3)
+			run("unpunish me")
+		end,
+		desc = "A better way of kicking"
+	},
+	["nok"] = {
+		name = "nok",
+		action = function()
+			for i, v in workspace.Terrain._Game.Workspace.Obby:GetDescendants() do
+				if v:IsA("TouchTransmitter") then
+					v:Destroy()
+				end
+			end
+		end,
+		desc = "Makes you immune to obby killbricks. *Can't be disabled until you rejoin*",
+		aliases = {"noobbykill"}
 	}
 }
 game:GetService("Players").LocalPlayer.Chatted:Connect(function(msg)
 	local user = msg:split(" ")
 	for i, v in commands do
-		if v.aliases then
-			for a, b in v.aliases do
-				if msg == b and user[2] then
-					v.action(user[2])
-					break
-				else
-					v.action()
-					break
-				end
-			end
-		elseif v.name == msg and user[2] then
+		if msg:find(settingsTable.prefix..v.name) and user[2] then
 			v.action(user[2])
-			break
-		else
+		elseif msg:find(settingsTable.prefix..v.name) and not user[2] then
 			v.action()
-			break
 		end
 	end
 end)
 
 local assets = workspace:FindFirstChildOfClass("Terrain"):WaitForChild("_Game")
-local pads = assets:WaitForChild("Pads")
+local pads = assets:WaitForChild("Admin"):WaitForChild("Pads")
 
-if settingsTable.actor_missing then
-	game:GetService("RunService").Heartbeat:ConnectParallel(function(dt)
+if settingsTable.actor_missing then -- this was meant to be used for ConnectParallel but there are some limitations that made those functions not work
+	game:GetService("RunService").Heartbeat:Connect(function(dt)
 		for i, v in settingsTable.banned do
-			if v:IsDescendantOf(game) and not v.Parent:IsA("Lighting") then
+			if v.Parent and not v.Parent:IsA("Lighting") then
+				local name = v.Name
 				run("punish "..name)
 				run("explode "..name)
 				run("name "..name.." Dangerous creature")
 			end
 		end
 	end)
-	game:GetService("RunService").Heartbeat:ConnectParallel(function(dt)
+	game:GetService("RunService").Heartbeat:Connect(function(dt)
 		if settingsTable.perm then
-			if not pads:FindFirstChild(game:GetService("Players").LocalPlayer.Name.."'s admin") and pads:FindFirstChild("Touch to get admin") then
+			if not pads:FindFirstChild(game.Players.LocalPlayer.Name.."'s admin") and pads:FindFirstChild("Touch to get admin") then
 				pcall(function()
-					firetouchinterestc(pads:FindFirstChild("Touch to get admin").Head, game:GetService("Players").LocalPlayer.Character.Torso, 1337)
+					firetouchinterestc(pads:FindFirstChild("Touch to get admin").Head, game.Players.LocalPlayer.Character.Torso, 1337)
 				end)
 			end
 		end
 	end)
 else
+	-- run_on_actor was meant to be used for ConnectParallel but there are some limitations that made those functions not work
 	run_on_actor(function()
-		game:GetService("RunService").Heartbeat:ConnectParallel(function(dt)
+		game:GetService("RunService").Heartbeat:Connect(function(dt)
 			for i, v in settingsTable.banned do
-				if v:IsDescendantOf(game) and not v.Parent:IsA("Lighting") then
+				if v.Parent and v.Parent.Name ~= "Lighting" then
+					local name = v.Name
 					run("punish "..name)
 					run("explode "..name)
 					run("name "..name.." Dangerous creature")
@@ -308,11 +432,11 @@ else
 		end)
 	end)
 	run_on_actor(function()
-		game:GetService("RunService").Heartbeat:ConnectParallel(function(dt)
+		game:GetService("RunService").Heartbeat:Connect(function(dt)
 			if settingsTable.perm then
-				if not pads:FindFirstChild(game:GetService("Players").LocalPlayer.Name.."'s admin") and pads:FindFirstChild("Touch to get admin") then
+				if not pads:FindFirstChild(game.Players.LocalPlayer.Name.."'s admin") and pads:FindFirstChild("Touch to get admin") then
 					pcall(function()
-						firetouchinterestc(pads:FindFirstChild("Touch to get admin").Head, game:GetService("Players").LocalPlayer.Character.Torso, 1337)
+						firetouchinterestc(pads:FindFirstChild("Touch to get admin").Head, game.Players.LocalPlayer.Character.Torso, 1337)
 					end)
 				end
 			end
@@ -323,5 +447,7 @@ game:GetService("StarterGui"):SetCore("SendNotification", {
 	Title = "Welcome to Kohls+",
 	Text = "Script loaded successfully"
 })
-
--- By using this script you automatically agree to the license which can be found at https://github.com/trollfacenan/random-kah-scripts/blob/main/LICENSE
+game:GetService("StarterGui"):SetCore("SendNotification", {
+	Title = "Aliases disabled",
+	Text = "Aliases are temporarily disabled because they were broken."
+})
