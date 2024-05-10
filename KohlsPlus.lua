649e3bb5628faf8bd112e967f55200b8dedd21ca
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2024-05-10 23:04:27 +0500

    Update KohlsPlus.lua

--- KohlsPlus.lua
+++ KohlsPlus.lua
@@ -13,7 +13,7 @@ local settingsTable = {
 	firsttimenotification = true,
 	banned = {},
 	connections = {},
-	no = {"9gn", "Not_Wojtek", "IHaveNoLifePF", "reefbro"},
+	no = {"9gn", "Not_Wojtek", "reefbro"},
 	actor_missing = true -- always true
 }
 
