0484c1133f4ac6d3737c53908a27ed198e6f17e7
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2024-05-17 17:33:46 +0500

    Update and rename kohls server crash.lua to silcrash.lua

---
+++ silcrash.lua
@@ -0,0 +1,10 @@
+for i = 1, 100 do
+	game:GetService("Players"):Chat("dog all all")
+end	
+for i = 1, 100 do
+	game:GetService("Players"):Chat("clone all all all")
+end
+repeat
+	game:GetService("Players"):Chat("clone all all all")
+	task.wait()
+until nil	
