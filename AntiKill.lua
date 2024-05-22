10af24ba50ee0a2268e7bb77711d44408720be92
thetrollfacenan <102139586+trollfacenan@users.noreply.github.com>
2024-05-22 22:56:06 +0500

    Update AntiKill.lua

--- AntiKill.lua
+++ AntiKill.lua
@@ -6,7 +6,10 @@ end
 game:GetService("RunService").RenderStepped:Connect(function()
 	local char = lp.Character or lp.CharacterAdded:Wait()
 	repeat game:GetService("RunService").RenderStepped:Wait() until lp.Character:FindFirstChild("Humanoid")
-	if lp.Character:FindFirstChild("Humanoid") and lp.Character.Humanoid.Health == 0 then
-		chat(":refresh me")
+	if lp.Character:FindFirstChild("Humanoid") and lp.Character:FindFirstChild("HumanoidRootPart") and lp.Character.Humanoid.Health == 0 then
+		local lastpos = lp.Character.HumanoidRootPart.CFrame
+		chat(":respawn me")
+		task.wait(0.75)
+		pcall(function() lp.Character.HumanoidRootPart.CFrame = lastpos end)
 	end
 end)
